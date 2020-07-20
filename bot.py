b8a4ee6160ee46a4325fda4082289541217f28f7
burgerhex <aviv.shai@gmail.com>
2020-07-19 21:30:00 -0700

    Add test commands and helper functions file

--- bot.py
+++ bot.py
@@ -1,30 +1,79 @@
 import discord
 import os
+import random
+from helpers import *
+
 
 client = discord.Client()
-
-is_prod = os.environ.get('IS_HEROKU', None)
-
-if not is_prod:
-    print("Not in Heroku, no token, aborting...")
-    exit(1)
-
-
 TOKEN = os.environ.get("BOT_TOKEN")
 
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print(f"Successfully logged in as {client.user} with ID {client.user.id}")
 
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
+    guild = message.guild
+    channel = message.channel
+    send = channel.send # this is a function
+    author = message.author
+    msg = message.content
+    id = message.id
+
+    # don't respond to self, don't respond to non-commands, and don't respond to DMs
+    if author == client.user or not is_command(msg) or not guild:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    print(f"Received command attempt \"{msg}\" from {author} in {channel} in {guild}")
+
+    parts = extract_parts(msg)
+    cmd = parts[0]
+    args = parts[1:]
+
+
+    if cmd in ["hello", "hi", "hey"]:
+        await send(f"Hello, {author.mention}! :smile:")
+
+
+    elif cmd in ["where", "whereami"]:
+        await send(f"We are in the server **{guild.name}** in the channel {channel.mention}! :smile:")
+
+
+    elif cmd in ["die", "dice"]:
+        await send(f"Rolled a {random.choice(range(1, 6))}.")
+
+
+    elif cmd in ["coin", "coinflip"]:
+        await send(f"Flipped {'Heads' if (random.random() > 0.5) else 'Tails'}.")
+
+
+    elif cmd in ["game"]:
+        overwrites = {
+            guild.default_role: discord.PermissionOverwrite(read_messages=False),
+            author: discord.PermissionOverwrite(read_messages=True)
+        }
+
+        name = " ".join(args) if args else 1
+
+        category = await guild.create_category(f"Poker Game {name}", overwrites=overwrites)
+        game = await guild.create_text_channel("game", category=category)
+        chat = await guild.create_text_channel("chat", category=category)
+        voice = await guild.create_voice_channel("voice", category=category)
+
+        await send("Created category and channels!")
+
+
+    elif cmd in ["delete", "del"]:
+        for channel in guild.channels[::-1]:
+            if isinstance(channel, discord.CategoryChannel) and channel.name.lower().startswith("poker game"):
+                for subchannel in channel.channels[::-1]:
+                    await subchannel.delete()
+                await channel.delete()
+
+        await send("Deleted game channels!")
+
 
 
 client.run(TOKEN)
---
+++ helpers.py
@@ -0,0 +1,22 @@
+
+COMMAND_START = "!"
+
+
+def is_command(msg):
+    return msg.startswith(COMMAND_START)
+
+
+def extract_parts(command):
+    assert command.startswith(COMMAND_START), "can't extract parts from non-command message"
+
+    # remove command start
+    command = command[len(COMMAND_START):]
+
+    # split by space, remove empties, and return
+    parts = command.split(" ")
+
+    while "" in parts:
+        parts.remove("")
+
+    return parts
+
