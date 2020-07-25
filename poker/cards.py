38d57d8a7725ba7f3680b8ca87981fa230238853
burgerhex <aviv.shai@gmail.com>
2020-07-24 18:58:35 -0700

    Added cards/poker hands and updated requirements

---
+++ poker/cards.py
@@ -0,0 +1,78 @@
+from enum import Enum
+from helpers import capitalize
+from random import random
+from math import floor
+
+
+NUM_SUITS = 4
+NUM_RANKS = 13
+CARDS_IN_DECK = 52
+
+
+# enums because i'm extra and want to emulate java :)
+class Suit(Enum):
+    DIAMONDS, HEARTS, CLUBS, SPADES = range(NUM_SUITS)
+
+
+class Rank(Enum):
+    ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, \
+        NINE, TEN, JACK, QUEEN, KING = range(NUM_RANKS)
+
+
+class Card:
+    def __init__(self, s: Suit, r: Rank):
+        assert isinstance(s, Suit) and isinstance(r, Rank), \
+            "must pass in Suit and Rank objects to initialize a Card"
+        self.suit = s
+        self.rank = r
+
+
+    def __str__(self):
+        return capitalize(self.rank.name) + " of " + capitalize(self.suit.name)
+
+
+    def __lt__(self, other):
+        assert isinstance(other, Card), "operator < must be used with two Cards"
+        if self.rank != other.rank:
+            return self.rank.value < other.rank.value
+        else:
+            return self.suit.value < other.suit.value
+
+
+    def __gt__(self, other):
+        assert isinstance(other, Card), "operator > must be used with two Cards"
+        if self.rank != other.rank:
+            return self.rank.value > other.rank.value
+        else:
+            return self.suit.value > other.suit.value
+
+
+    def __eq__(self, other):
+        assert isinstance(other, Card), "operator == must be used with two Cards"
+        return not (self < other or self > other)
+
+
+class Deck:
+    def __init__(self):
+        self.cards = []
+
+        for s in Suit:
+            for r in Rank:
+                self.cards.append(Card(s, r))
+
+        assert len(self.cards) == NUM_SUITS * NUM_RANKS == CARDS_IN_DECK, "deck is invalid"
+
+
+    def shuffle(self):
+        for i in range(len(self.cards)):
+            j = floor(random() * len(self.cards))
+            self.cards[i], self.cards[j] = self.cards[j], self.cards[i]
+        return self
+
+
+    def deal(self):
+        return self.cards.pop() if self.cards else None
+
+
+    def __str__(self):
+        return str(list(map(str, self.cards)))
---
+++ poker/hands.py
@@ -0,0 +1,239 @@
+# all of these functions return False if the associated hand is not present,
+# and return the cards that create the hand if it is present
+from enum import Enum
+from poker.cards import Rank, Deck
+from helpers import capitalize
+# import tqdm
+
+NUM_HANDS = 10
+
+
+def has_royal_flush(hand):
+    if len(hand) < 5:
+        return False
+
+    s_flush = has_straight_flush(hand)
+    if not s_flush:
+        return False
+
+    ranks = [c.rank for c in s_flush]
+
+    if Rank.KING in ranks and Rank.ACE in ranks:
+        return s_flush
+    else:
+        return False
+
+
+def has_straight_flush(hand):
+    if len(hand) < 5:
+        return False
+
+    flush = has_flush(hand)
+    if not flush:
+        return False
+
+    suit = flush[0].suit
+    hand_suit = [c for c in hand if c.suit == suit]
+
+    straight = has_straight(hand_suit)
+    if not straight:
+        return False
+
+    return straight
+
+
+def has_four_kind(hand):
+    return has_group(hand, 4)
+
+
+def has_full_house(hand):
+    if len(hand) < 5:
+        return False
+
+    three = has_three_kind(hand)
+    if not three:
+        return False
+
+    rank3 = three[0].rank
+    hand_no3 = [c for c in hand if c.rank != rank3]
+
+    pair = has_pair(hand_no3)
+    if not pair:
+        return False
+
+    return three + pair
+
+
+def has_flush(hand):
+    if len(hand) < 5:
+        return False
+
+    suits = [c.suit.value for c in hand]
+    appearances = {suit: suits.count(suit) for suit in set(suits)}
+    flushes = {suit: count for suit, count in appearances.items() if count >= 5}
+
+    if len(flushes) == 0:
+        return False
+
+    hand_s = list(sorted(hand, key=lambda c: -c.rank.value))
+    # move aces to front
+    num_aces = len([c for c in hand_s if c.rank == Rank.ACE])
+    hand_s = hand_s[-num_aces:] + hand_s[:-num_aces]
+
+    for card in hand_s:
+        if card.suit.value in flushes:
+            this_suit = [c for c in hand_s if c.suit == card.suit]
+            return this_suit[:5]
+
+
+def has_straight(hand):
+    if len(hand) < 5:
+        return False
+
+    ranks = [c.rank.value for c in hand]
+    ranks_s = sorted(set(ranks))
+
+    for i in range(len(ranks_s) - 3)[::-1]:
+        if (i < len(ranks_s) - 4 and all([ranks_s[i + j] + 1 == ranks_s[i + j + 1] for j in range(4)])) or \
+                (all([ranks_s[i + j] == Rank.TEN.value + j for j in range(4)]) and Rank.ACE.value in ranks_s):
+            # have to check 10-A straight separately ^
+            lowest = ranks_s[i]
+            to_get = list(range(lowest, lowest + 5)) if lowest != Rank.TEN.value \
+                else [*range(Rank.TEN.value, Rank.TEN.value + 4), Rank.ACE.value]
+
+            cards = []
+            for c in hand:
+                if c.rank.value in to_get:
+                    cards.append(c)
+                    to_get.remove(c.rank.value)
+
+            cards = list(sorted(cards, key=lambda c: c.rank.value))
+            if lowest == Rank.TEN.value:
+                cards = cards[1:] + cards[:1]
+
+            return cards
+
+    return False
+
+
+def has_three_kind(hand):
+    return has_group(hand, 3)
+
+
+def has_two_pair(hand):
+    if len(hand) < 4:
+        return False
+
+    ranks = [c.rank.value for c in hand]
+    appearances = {rank: ranks.count(rank) for rank in set(ranks)}
+    pairs = {rank: count for rank, count in appearances.items() if count >= 2}
+
+    if len(pairs) < 2:
+        return False
+
+    pair_ranks = sorted(pairs.keys())[::-1]
+    highest1, highest2 = pair_ranks[:2]
+
+    cards1 = [c for c in hand if c.rank.value == highest1]
+    cards2 = [c for c in hand if c.rank.value == highest2]
+
+    return cards1[:2] + cards2[:2]
+
+
+def has_pair(hand):
+    return has_group(hand, 2)
+
+
+# detects pair (two of a kind), three of a kind, and four of a kind all in one
+def has_group(hand, group_size):
+    if len(hand) < group_size:
+        return False
+
+    ranks = [c.rank.value for c in hand]
+    appearances = {rank: ranks.count(rank) for rank in set(ranks)}
+    groups = {rank: count for rank, count in appearances.items() if count >= group_size}
+
+    if len(groups) == 0:
+        return False
+
+    group_ranks = sorted(groups.keys())[::-1]
+    highest_rank = Rank.ACE.value if Rank.ACE.value in group_ranks else group_ranks[0]
+
+    cards_of_rank = [c for c in hand if c.rank.value == highest_rank]
+
+    return cards_of_rank[:group_size]
+
+
+class PokerHand(Enum):
+    HIGH_CARD, PAIR, TWO_PAIR, THREE_KIND, STRAIGHT, FLUSH, FULL_HOUSE, \
+        FOUR_KIND, STRAIGHT_FLUSH, ROYAL_FLUSH = range(NUM_HANDS)
+
+
+def detect_hand(hand):
+    hand_funcs = [has_royal_flush, has_straight_flush, has_four_kind, has_full_house,
+                  has_flush, has_straight, has_three_kind, has_two_pair, has_pair]
+
+    for hand_num in range(NUM_HANDS):
+        if hand_num == NUM_HANDS - 1:
+            if Rank.ACE in [c.rank for c in hand]:
+                m = [c for c in hand if c.rank == Rank.ACE][0]
+            else:
+                m = max(hand, key=lambda c: c.rank.value)
+
+            return PokerHand.HIGH_CARD, [m]
+
+        func = hand_funcs[hand_num]
+        found = func(hand)
+        if found:
+            return PokerHand(NUM_HANDS - hand_num - 1), found
+
+
+# TEST 1 - testing that the functions work
+# hands = 0
+# while True:
+#     HAND_SIZE = 7
+#     HAND_FUNC = has_pair
+#
+#     deck = Deck().shuffle()
+#     hand = [deck.deal() for i in range(HAND_SIZE)]
+#     hands += 1
+#     found = HAND_FUNC(hand)
+#     if found:
+#         print("hand: ", end='')
+#         print([str(c) for c in sorted(hand, key=lambda c: c.rank.value)])
+#         print("found: ", end='')
+#         print([str(c) for c in found])
+#         print(f"{hands} hands searched")
+#         break
+
+# TEST 2 - more of a simulation really
+# hands = 1000000
+# HAND_SIZE = 7
+# hand_types = ["rf", "sf", "4k", "fh", "fl", "st", "3k", "2p", "pr"]
+# hand_funcs = [has_royal_flush, has_straight_flush, has_four_kind, has_full_house,
+#               has_flush, has_straight, has_three_kind, has_two_pair, has_pair]
+# found_hands = {typ: 0 for typ in hand_types}
+# for _ in tqdm.tqdm(range(hands)):
+#     deck = Deck().shuffle()
+#     hand = [deck.deal() for _ in range(HAND_SIZE)]
+#
+#     for i in range(len(hand_types)):
+#         found = hand_funcs[i](hand)
+#         if found:
+#             found_hands[hand_types[i]] += 1
+#
+# print(found_hands)
+
+# TEST 3 - testing detection of hands
+NUM_TESTS = 5
+HAND_SIZE = 7
+for _ in range(NUM_TESTS):
+    deck = Deck().shuffle()
+    hand_ = [deck.deal() for i in range(HAND_SIZE)]
+
+    hand_type, hand_cards = detect_hand(hand_)
+
+    print("Cards:      " + ", ".join([str(c) for c in hand_]))
+    print("Hand found: " + capitalize(hand_type.name.replace("_", " ")))
+    print("Hand cards: " + ", ".join([str(c) for c in hand_cards]))
+    print()
